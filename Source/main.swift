743136e7a2c2b3493c5960db0bc2e08a48c9e04c
Luna Razzaghipour <luna@xoria.org>
2025-05-06 20:51:26 +1000

    Let the user reopen the window by opening the app again

--- Source/main.swift
+++ Source/main.swift
@@ -6,15 +6,19 @@ NSApp.delegate = appDelegate
 NSApp.run()
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-	var window: NSWindow! = nil
-
-	func applicationDidFinishLaunching(_ notification: Notification) {
-		window = NSWindow(
+	lazy var window = {
+		let w = NSWindow(
 			contentRect: .init(x: 0, y: 0, width: 200, height: 100),
 			styleMask: [.titled, .closable, .miniaturizable, .resizable],
 			backing: .buffered,
 			defer: true,
 		)
+		w.isReleasedWhenClosed = false
+		return w
+	}()
+
+	func applicationOpenUntitledFile(_ sender: NSApplication) -> Bool {
 		window.makeKeyAndOrderFront(nil)
+		return true
 	}
 }
