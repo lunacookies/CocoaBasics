92d689c7e41207d6554f47f5bc2d5f3c3c3f9464
Luna Razzaghipour <luna@xoria.org>
2025-05-06 20:51:03 +1000

    Show window on launch

--- Source/main.swift
+++ Source/main.swift
@@ -6,7 +6,15 @@ NSApp.delegate = appDelegate
 NSApp.run()
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-	func applicationWillTerminate(_ notification: Notification) {
-		print("terminating!")
+	var window: NSWindow! = nil
+
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		window = NSWindow(
+			contentRect: .init(x: 0, y: 0, width: 200, height: 100),
+			styleMask: [.titled, .closable, .miniaturizable, .resizable],
+			backing: .buffered,
+			defer: true,
+		)
+		window.makeKeyAndOrderFront(nil)
 	}
 }
